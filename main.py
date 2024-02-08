4914000335b8474440af0daaf99e3842847396c5
Valerii <valeri.github@gmail.com>
2024-02-08 17:06:02 +0300

    Create image notes

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from schemas import Note, NoteList
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")      # Шаблоны
-
+notes_list = NoteList()
 
 @app.get("/", response_class=HTMLResponse)
 async def get_home_page(request: Request):
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "notes_count": 4444     # TODO: количество записок
+        "notes_count": len(notes_list.all_notes)    
     })
