010a06bbf1deb98239f3642ac0b52ec527d7a50e
Valerii <valeri.github@gmail.com>
2024-02-08 16:26:58 +0300

    Add app and templates

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")      # Шаблоны
+
