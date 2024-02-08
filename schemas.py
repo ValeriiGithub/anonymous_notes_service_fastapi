56f18ca637f1f5194a7eedc0895cf88673502f6e
Valerii <valeri.github@gmail.com>
2024-02-08 16:43:44 +0300

    Create schemas

---
+++ schemas.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Note(BaseModel):
+    text: str
+    secret: str
+    note_hash: str = None   # Результат, ссылка на готовую записку
+
+
+class NoteList(BaseModel):
+    all_notes: List[Note] = list
+
+
+# class NoteID(BaseModel):
+#     note_id: str
+#     note_secret: str
