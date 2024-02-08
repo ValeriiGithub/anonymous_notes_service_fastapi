e5f3df025d660c9fee30a7e058199c2e991aa8fd
Valerii <valeri.github@gmail.com>
2024-02-08 16:37:02 +0300

    Add response index.html

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ templates = Jinja2Templates(directory="templates")      # Шаблоны
 
 @app.get("/", response_class=HTMLResponse)
 async def get_home_page(request: Request):
-    return "test"
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "notes_count": 4444     # TODO: количество записок
+    })
