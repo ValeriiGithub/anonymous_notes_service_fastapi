964b6a2b7b58c79d6f7d477e8e4aa94261475e18
Valerii <valeri.github@gmail.com>
2024-02-08 16:29:11 +0300

    Create function get_home_page

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")      # Шаблоны
 
+
+@app.get("/", response_class=HTMLResponse)
+async def get_home_page(request: Request):
+    return "test"
